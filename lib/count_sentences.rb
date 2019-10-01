19d2d3d23f40033517e87f098f40fc6bac24b798
Daniel Matney <danieljmatney@gmail.com>
2019-10-01 03:25:06 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -21,18 +21,6 @@ class String
         sentences.delete(word)
       end
     end
-    sentences = sentences.split("?")
-    sentences.each do |word|
-      if word == nil
-        sentences.delete(word)
-      end
-    end
-    sentences = sentences.split("!")
-    sentences.each do |word|
-      if word == nil
-        sentences.delete(word)
-      end
-    end
     sentences.length
   end
 end

2437e376ea5b4b634551b748fdfaeb0a5f9d87c6
Daniel Matney <danieljmatney@gmail.com>
2019-10-01 04:02:06 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -21,7 +21,7 @@ class String
     sentences = sentences.split("!")
     sentences = sentences.join(".")
     sentences = sentences.split(".")
-    sentences.reject {|word| if word == nil}
+    sentences.reject {|word| if word == nil end}
    # sentences1 = []
     #sentences2 = []
     #sentences.each do |fragment|
